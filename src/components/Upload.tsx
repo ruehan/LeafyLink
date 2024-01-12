67e404b90adb3e42e56453aca8948d09dd149852
vendredl <node.js_@kakao.com>
2024-01-13 01:43:37 +0900

    :speech_balloon: Add Image Preview in Upload Module

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -7,22 +7,34 @@ interface UploadProps{
 
 const Upload : React.FC<UploadProps> = ({ onUpload }) => {
     const [selectedImage, setSelectedImage] = useState<File | null>(null)
+    const [previewImage, setPreviewImage] = useState<string | null>(null)
 
     const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0]
-        setSelectedImage(file || null)
+
+       if(file){
+        const imageUrl = URL.createObjectURL(file);
+        setPreviewImage(imageUrl);
+        setSelectedImage(file);
+       }else{
+        setPreviewImage(null);
+        setSelectedImage(null);
+       }
+
+        
     }
 
     const handleUpload = () => {
         if(selectedImage){
-            console.log(selectedImage)
             alert(`Image Uploaded! [${selectedImage.name}]`)
+            onUpload(selectedImage)
         }
     }
 
     return (
         <div className={styles['upload-container']}>
             <input type="file" accept="image/*" onChange={handleImageChange} className={styles['upload-input']} />
+            {previewImage && <img src={previewImage} alt="Preview" style={{ maxWidth: '300px', maxHeight: '600px', marginTop: '10px' }} />}
             <button onClick={handleUpload} className={styles['upload-button']}>Upload Image</button>
         </div>
     )
