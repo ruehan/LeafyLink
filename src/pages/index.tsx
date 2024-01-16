c70a85b860c10e7ea87edf8ef57c5bacbe82e434
vendredl <node.js_@kakao.com>
2024-01-17 03:15:40 +0900

    :speech_balloon: Update UploadImage API EndpointConnection in Index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,26 @@ import styles from '../styles/Index.module.css';
 const Home = () => {
   const [analysisResult, setAnalysisResult] = useState<string | null>(null);
 
-  const handleUpload = (image: File) => {
+  const handleUpload = async (image: File) => {
     // 추후 알고리즘 결과 삽입
+
+    console.log(image)
+
+    const formData = new FormData();
+    formData.append('images', image);
+
+    const response = await fetch('/api/uploadImage', {
+      method: 'POST',
+      body: formData,
+    })
+
+    if(response.ok){
+      const result = await response.json();
+      console.log('Image Upload Result', result)
+    }else{
+      console.error('Image Upload Failed')
+    }
+
     setAnalysisResult('Healthy Plant');
   };
 
